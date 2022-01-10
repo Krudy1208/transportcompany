c5dec46c532a044f95bc9aa13e9253cc256cf725
Krudy1208 <97240299+Krudy1208@users.noreply.github.com>
2022-01-10 20:33:52 +0300

    Update connect.php

--- assets/vendor/connect.php
+++ assets/vendor/connect.php
@@ -1,12 +1,12 @@
 <?php
-$connect = mysqli_connect("rudy.zzz.com.ua", "roottransportvom", "Wert*5", "transportcom92");
+$connect = mysqli_connect("", "", "", "");
 if ($connect->connect_error) {
     die("Ошибка: " . mysqli_connect_error());
 }
 
 //*** Объектно-ориентированный стиль для подготовленных запросов ***
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$connect_oop = new mysqli("rudy.zzz.com.ua", "roottransportvom", "Wert*5", "transportcom92");
+$connect_oop = new mysqli("", "", "", "");
 if ($connect_oop->connect_error) {
     die("Ошибка: " . mysqli_connect_error());
 }
